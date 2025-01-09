50ae6fc79a7d588d5d84ba707a7fad7f3f8f155b
AndriyPavlyuk <andriypavlukwrc@gmail.com>
2025-01-09 15:34:42 +0200

    Add configuration for greeting message

--- src/main/java/com/polarbookshop/catalogservice/controller/BookController.java
+++ src/main/java/com/polarbookshop/catalogservice/controller/BookController.java
@@ -5,7 +5,6 @@ import com.polarbookshop.catalogservice.service.BookService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
